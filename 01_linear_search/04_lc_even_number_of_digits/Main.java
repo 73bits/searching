50c3bf7c314dc38b789ebc9fb4ed1242771acb78
73bits <prashantkr0628@gmail.com>
2025-12-11 19:57:54 +0530

    optimized using Math.log10

--- 01_linear_search/04_lc_even_number_of_digits/Main.java
+++ 01_linear_search/04_lc_even_number_of_digits/Main.java
@@ -2,6 +2,7 @@ public class Main {
 	public static void main(String ... args) {
 		int[] arr = new int[] {0, -12, 234, 2, 5, 7896};
 		System.out.println(findNumbers(arr));
+		System.out.println(findNumbersOptimized(arr));
 	}
 
 	public static int findNumbers(int[] arr) {
@@ -17,6 +18,18 @@ public class Main {
 				digits++;
 			}
 
+			if (digits % 2 == 0) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public static int findNumbersOptimized(int[] arr) {
+		int count = 0;
+		for (int num : arr) {
+			int digits = (num == 0) ? 1 :  (int)Math.log10(Math.abs(num)) + 1;
 			if (digits % 2 == 0) {
 				count++;
 			}
