f9a6c15d084c10426e5e9744b880680079fedd2f
73bits <prashantkr0628@gmail.com>
2025-12-13 01:16:38 +0530

    added recursive search()

--- 02_binary_search/00_main/Main.java
+++ 02_binary_search/00_main/Main.java
@@ -3,6 +3,7 @@ public class Main {
 		int[] arr = new int[] {-1, 2, 5, 8, 9, 16, 24, 98, 112, 456};
 		int target = 112;
 		System.out.println("target index: " + search(arr, target));
+		System.out.println("target index: " + search(arr, target, 0, arr.length));
 	}
 
 	private static int search(int[] arr, int target) {
@@ -15,5 +16,13 @@ public class Main {
 			if (arr[mid] < target) start = mid + 1;
 		}
 		return -1;
+	}
+
+	private static int search(int[] arr, int target, int start, int end) {
+		int mid = (start + end) / 2;
+		if (start > end) return -1;
+		if (arr[mid] == target) return mid;
+		if (arr[mid]  > target) return search(arr, target, start, mid - 1);
+		return search(arr, target, mid + 1, end);
 	}
 }
