ea152364704c0da769c52652f047b1e525cb6c72
73bits <prashantkr0628@gmail.com>
2025-12-15 17:35:20 +0530

    added floor() opposite to ceiling()

--- binary_search/CeilingFloor.java
+++ binary_search/CeilingFloor.java
@@ -3,6 +3,7 @@ public class CeilingFloor {
 		int[] arr = new int[] { 2, 3, 5, 9, 14, 16, 18 };
 		int target = 11;
 		System.out.println("target index: " + ceiling(arr, target));
+		System.out.println("target index: " + floor(arr, target));
 	}
 
 	private static int ceiling(int[] arr, int target) {
@@ -16,5 +17,17 @@ public class CeilingFloor {
 			if (arr[mid] > target) end = mid - 1;
 		}
 		return start;
+	}
+
+	private static int floor(int[] arr, int target) {
+		if (target < arr[0]) return -1;
+		int start = 0, end = arr.length - 1;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (arr[mid] == target) return mid;
+			if (arr[mid] < target) start = mid + 1;
+			if (arr[mid] > target) end = mid - 1;
+		}
+		return end;
 	}
 }
