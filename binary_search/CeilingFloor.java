ec2c6e08b9fd15b974acae385edb669fb3921430
73bits <prashantkr0628@gmail.com>
2025-12-15 17:04:59 +0530

    added condition for target > last element

--- binary_search/CeilingFloor.java
+++ binary_search/CeilingFloor.java
@@ -6,6 +6,8 @@ public class CeilingFloor {
 	}
 
 	private static int ceiling(int[] arr, int target) {
+		// might be possible that index[arr.length - 1] is smaller that target
+		if (target > arr[arr.length - 1]) return -1;
 		int start = 0, end = arr.length - 1;
 		while (start < end) {
 			int mid = (start + end) / 2;
