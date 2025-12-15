ca65ae9ee19d00e7e5d83c90f5589ad5fd989745
73bits <prashantkr0628@gmail.com>
2025-12-15 17:02:10 +0530

    added ceiling() for the targets

--- binary_search/CeilingFloor.java
+++ binary_search/CeilingFloor.java
@@ -1,4 +1,18 @@
 public class CeilingFloor {
 	public static void main(String ... args) {
+		int[] arr = new int[] { 2, 3, 5, 9, 14, 16, 18 };
+		int target = 11;
+		System.out.println("target index: " + ceiling(arr, target));
+	}
+
+	private static int ceiling(int[] arr, int target) {
+		int start = 0, end = arr.length - 1;
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (arr[mid] == target) return mid;
+			if (arr[mid] < target) start = mid + 1;
+			if (arr[mid] > target) end = mid - 1;
+		}
+		return start;
 	}
 }
