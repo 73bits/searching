ea13c0706cbd19bd24491d436061e2a9605e8933
73bits <prashantkr0628@gmail.com>
2025-12-17 21:50:11 +0530

    question added

--- binary_search/LCNextGreatest.java
+++ binary_search/LCNextGreatest.java
@@ -1,3 +1,9 @@
+/*
+   return the next greatest letter with wrap arround
+   letters['c', 'f', 'j', 'm', 'w', 'x']
+   target = 'f', return 'j'
+   target = 'x', return 'c'
+*/
 public class LCNextGreatest {
 	public static void main(String ... args) {
 	}
