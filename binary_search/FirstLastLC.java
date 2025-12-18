5d4b287eb541870262a7d2a7a640781791cc16e7
73bits <prashantkr0628@gmail.com>
2025-12-18 19:04:08 +0530

    question added

--- binary_search/FirstLastLC.java
+++ binary_search/FirstLastLC.java
@@ -1,3 +1,9 @@
+/*
+   first and last position of element in sorted array
+   nums = [5, 7, 7, 8, 8, 10]
+   target = 8, output = [3, 4]
+   target = 9, output = [-1, -1]
+ */
 public class FirstLastLC {
 	public static void main(String ... args) {
 	}
