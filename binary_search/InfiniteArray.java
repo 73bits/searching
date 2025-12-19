e4ef21712a2b71aa87a97a046a815e3bcc66cce0
73bits <prashantkr0628@gmail.com>
2025-12-19 22:54:07 +0530

    checked for start, end in log(n)

--- binary_search/InfiniteArray.java
+++ binary_search/InfiniteArray.java
@@ -6,6 +6,21 @@ public class InfiniteArray {
 	public static void main(String ... args) {
 	}
 
+	private static int searchInfiniteArray(int[] arr, int target) {
+		int start = 0, end = 1;
+		try {
+			while (arr[end] < target) {
+				int tmp = end + 1;
+				end = end + (end - start + 1) * 2;
+				start = tmp;
+			}
+			return binarySearch(arr, target, start, end);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("not an infinite array ;)");
+		}
+		return -1;
+	}
+
 	private static int binarySearch(int[] arr, int target, int start, int end) {
 		while(start <= end) {
 			int mid = (start + end) / 2;
