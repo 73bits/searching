c5a77013e37f91641934187430a132a9f6fa11b8
73bits <prashantkr0628@gmail.com>
2025-12-19 22:51:58 +0530

    added binarySearch(arr, target, start, int)

--- binary_search/InfiniteArray.java
+++ binary_search/InfiniteArray.java
@@ -5,4 +5,14 @@
 public class InfiniteArray {
 	public static void main(String ... args) {
 	}
+
+	private static int binarySearch(int[] arr, int target, int start, int end) {
+		while(start <= end) {
+			int mid = (start + end) / 2;
+			if (arr[mid] < target) start = mid + 1;
+			else if (arr[mid] > target) end = mid - 1;
+			else return mid;
+		}
+		return -1;
+	}
 }
