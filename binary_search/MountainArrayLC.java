bd259c4d727b133041745c8aa0bb58e911ab0b7f
73bits <prashantkr0628@gmail.com>
2025-12-20 21:47:48 +0530

    question added

--- binary_search/MountainArrayLC.java
+++ binary_search/MountainArrayLC.java
@@ -1,3 +1,8 @@
+/*
+   find peak index in mountain array: arr.length >= 3, O(log n)
+   arr = [0, 1, 0] output: 1
+   arr = [0, 2, 1] output: 1
+*/
 public class MountainArrayLC {
 	public static void main(String ... args) {
 	}
